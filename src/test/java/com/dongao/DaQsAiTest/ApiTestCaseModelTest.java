10895a3d1a74218ef34331d77b7e4cfe2566760e
huiqi <huiqi0920@163.com>
2021-02-07 14:18:41 +0800

    更新用例

--- src/test/java/com/dongao/DaQsAiTest/ApiTestCaseModelTest.java
+++ src/test/java/com/dongao/DaQsAiTest/ApiTestCaseModelTest.java
@@ -26,8 +26,8 @@ class ApiTestCaseModelTest {
             return responseBuilder.build();
         });
         baseApi=new BaseApi();
-        baseApi.load("src/main/resources/com.dongao.DaQsAiTest/api/V1/exam");
-        apiTestCase=ApiTestCaseModel.load("src/main/resources/com.dongao.DaQsAiTest/case/V1/exam/qs_exam_unCollectQues.yaml");
+        baseApi.load("src/main/resources/com.dongao.DaQsAiTest/api/V3/marketingMessage");
+        apiTestCase=ApiTestCaseModel.load("src/main/resources/com.dongao.DaQsAiTest/case/V3/marketingMessage/qs_marketingMessage_getMarketingMessageDetail.yaml");
     }
 
     @Test
