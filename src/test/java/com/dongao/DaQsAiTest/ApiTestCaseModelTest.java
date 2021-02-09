c7175990332af223ab2ce2c854fed5e80cf0df4d
christbella <308667244@qq.com>
2021-02-09 09:36:47 +0800

    修改bug

--- src/test/java/com/dongao/DaQsAiTest/ApiTestCaseModelTest.java
+++ src/test/java/com/dongao/DaQsAiTest/ApiTestCaseModelTest.java
@@ -26,8 +26,8 @@ class ApiTestCaseModelTest {
             return responseBuilder.build();
         });
         baseApi=new BaseApi();
-        baseApi.load("src/main/resources/com.dongao.DaQsAiTest/api/V1/exam");
-        apiTestCase=ApiTestCaseModel.load("src/main/resources/com.dongao.DaQsAiTest/case/V1/exam/qs_exam_unCollectQues.yaml");
+        baseApi.load("src/main/resources/com.dongao.DaQsAiTest/api/V1/class");
+        apiTestCase=ApiTestCaseModel.load("src/main/resources/com.dongao.DaQsAiTest/case/V1/class/qs_class_encourage.yaml");
     }
 
     @Test
